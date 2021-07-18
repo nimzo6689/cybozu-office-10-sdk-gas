311eeafde089beaa7f3379f05959f347b8ae1ceb
nimzo6689 <nimzo.i.d.6689@gmail.com>
2021-07-19 07:53:26 +0900

    fix: build 時の Unexpected token error の解消

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         ],
       },
