ed18b38f3f5479288d877c6ac74b892af50b9b06
nimzo6689 <nimzo.i.d.6689@gmail.com>
2024-02-27 06:26:30 +0900

    Upgrade jest libraries

--- __tests__/Helpers.spec.ts
+++ __tests__/Helpers.spec.ts
@@ -1,7 +1,9 @@
 import { Utils } from '../src/common/Helpers';
 
-test('test 1.', () => {
-  expect(Utils.createCookieValue('xxx')).toBe(
-    'CBAccount=; expires=Thu, 01-Jan-1970 00:00:00 GMT; path=/cgi-bin/cbag/; AGSESSID=xxx; path=/cgi-bin/cbag/; secure; HttpOnly'
-  );
+describe('ヘルパー関数', () => {
+  it('クッキー文字列から AGSESSID の値を取得できるか', () => {
+    expect(Utils.createCookieValue('xxx')).toBe(
+      'CBAccount=; expires=Thu, 01-Jan-1970 00:00:00 GMT; path=/cgi-bin/cbag/; AGSESSID=xxx; path=/cgi-bin/cbag/; secure; HttpOnly'
+    );
+  });
 });
